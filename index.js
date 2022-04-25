56821ec3450fe238a34d5ff8d99d0dafdb2c23ef
Isaiah <ijandicos90@gmail.com>
2022-04-24 22:16:12 -0400

    Completed Assigment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+  return  input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+
 }
+
+
